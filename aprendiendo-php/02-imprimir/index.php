041ac51938ed7e04eb40d0d00d75e2aba2e76cd5
LuisRcap <luisdany7773@gmail.com>
2021-06-25 19:53:10 -0500

    Imprimir en pantalla

---
+++ aprendiendo-php/02-imprimir/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+
+<html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Imprimir por pantalla - Master en PHP</title>
+    </head>
+    
+    <body>
+        <h1>Master en PHP</h1>
+        
+        <?php
+            echo '<h3>Listado de videojuegos</h3>';
+            echo '<ul>'
+                    .'<li>GTA</li>'
+                    .'<li>FIFA</li>'
+                    .'<li>Mario Bros</li>'
+                    .'</ul>';
+            echo '<p>Esta es toda'.' - '.'lista de videojuegos</p>';
+                    
+        ?>
+    </body>
+</html>
