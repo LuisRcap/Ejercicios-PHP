b58ed4b17e1578b87e0c74022e30c6c174b1f70f
LuisRcap <luisdany7773@gmail.com>
2021-06-25 20:01:27 -0500

    Comentarios

--- aprendiendo-php/02-imprimir/index.php
+++ aprendiendo-php/02-imprimir/index.php
@@ -9,13 +9,25 @@
     <body>
         <h1>Master en PHP</h1>
         
+        <?="Bienvenido al curso más grande de PHP"?>
+        
         <?php
+            // Titular de la sección
             echo '<h3>Listado de videojuegos</h3>';
+            
+            /* Esta es una lista
+             * de videojuegos
+             * moderno
+             */
             echo '<ul>'
                     .'<li>GTA</li>'
                     .'<li>FIFA</li>'
                     .'<li>Mario Bros</li>'
                     .'</ul>';
+            
+            //echo '<br/>HOLA HOLA HOLA<br/>';
+            
+            // Párrado explicativo
             echo '<p>Esta es toda'.' - '.'lista de videojuegos</p>';
                     
         ?>
